58ffe82a21d8ec43984d9fb10ceb830d477a6b2e
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-10-06 22:31:50 +0530

    Create Find the number of islands.cpp

---
+++ Find the number of islands.cpp
@@ -0,0 +1,13 @@
+// CodeGenius
+        n=grid.size();
+        m=grid[0].size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    solve(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
